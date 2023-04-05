0640300c6f22580af4568993175bd047a1ff9c89
ISMAIL AMAAN <kefey6iaa@nottingham.edu.my>
2023-04-05 05:29:24 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,83 @@
+let money = 0;
+
+let copperMineBought = false;
+let copperMiner = 1;
+let copper = 1;
+let copperCost = 5;
+
+let count = 1000;
+let seconds = count/1000;
+
+console.log(copperMineBought);
+
+function buyCopperMine(){
+    
+    copperMineBought = true;
+    console.log(copperMineBought);
+    showText();
+    showButtons();
+   
+}
+
+function showButtons(){
+    document.getElementById("upgradeBatch1").style.display="inline";
+}
+
+function mineCopper(){
+    copper += 1;
+    document.getElementById("copper").innerHTML = "Copper count: " + copper;
+}
+
+function sellCopper1(){
+        
+        money += copperCost*copper;
+        copper -= copper;
+        document.getElementById("money").innerHTML = "money: $" + money;
+        document.getElementById("copper").innerHTML = "Copper count: " + copper;
+    
+}
+
+function sellCopper2(){
+    if (copper >= 2){
+    money += copperCost*(copper/2);
+    copper -= (copper/2);
+    document.getElementById("money").innerHTML = "money: $" + money;
+    document.getElementById("copper").innerHTML = "Copper count: " + copper;
+    }
+
+}
+
+function buyMiner(){
+    if (money >= 150){
+        money -= 150;
+        copperMiner += 1;
+        document.getElementById("money").innerHTML = "money: $" + money;
+
+    }
+}
+
+function showText(){
+    document.getElementById("copperMineTitle").innerHTML = "COPPER MINE (" + 1*copperMiner/seconds + " copper/s)";
+    document.getElementById("copper").innerHTML = "Copper count: " + copper;
+    document.getElementById("copperMiners").innerHTML = "Miners: " + copperMiner;
+}
+
+function copperMine(){
+        if (copperMiner){
+            return copper += 1*copperMiner;
+        }
+       
+}
+
+setInterval(function() {
+    if (copperMineBought){
+        copperMine(copperMiner);
+        showText();
+    }
+    
+    
+
+}, count)
+
+
+  
---
+++ js/ore.js
@@ -0,0 +1,19 @@
+let copperMiner = 1;
+let copper = 1;
+
+export function copperMine(copperMiner){
+    if (copperMiner){
+        return copper += 1*copperMiner;
+    }
+
+
+}
+
+setInterval(function() {
+    //copperMine();
+
+    
+   // console.log(copper);
+  }, 1000) // Run every second; 1 second == 1000 milliseconds
+
+//export {copper, copperMiner};
